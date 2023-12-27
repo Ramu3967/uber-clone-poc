bea3dfb6d55a21122bd6e5ee1ac62425ad5592c8
mramu_1996 <mandaloju.ramu@gmail.com>
2023-12-27 18:16:14 -0500

    tested the firebase integration (signup and login) - View Model and Hilt to be introduced.

---
+++ app/src/main/java/com/example/uberclone/ui/fragments/RiderFragment.kt
@@ -0,0 +1,13 @@
+package com.example.uberclone.ui.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.uberclone.R
+
+class RiderFragment: Fragment(R.layout.fragment_rider) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/example/uberclone/utils/UberConstants.kt
@@ -0,0 +1,6 @@
+package com.example.uberclone.utils
+
+object UberConstants  {
+    const val RIDER = "Rider"
+    const val DRIVER = "Driver"
+}
